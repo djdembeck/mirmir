7f7c727d8d6190f0503b43e41fa59e1f48d53b25
djdembeck <dembeck.david@protonmail.com>
2021-07-01 17:24:47 -0500

    Rename project to "Mirmir"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "mb_track_credit_seeder_production"
+  # config.active_job.queue_name_prefix = "mirmir_production"
 
   config.action_mailer.perform_caching = false
 
